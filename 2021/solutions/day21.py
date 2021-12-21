05d3a3f7555d99d49c3259d7104317b7187c20a2
Perruccio <francesco.perinti@gmail.com>
2021-12-21 16:52:04 +0100

    add day21 recursion

--- 2021/solutions/day21.py
+++ 2021/solutions/day21.py
@@ -3,6 +3,9 @@ import pathlib
 prj_path = str(pathlib.Path(__file__).parent.parent.resolve())
 sys.path.append(prj_path)
 from utils.aoc import *
+from functools import lru_cache, reduce
+from itertools import product
+from collections import Counter
 
 def part1(p1, p2):
     def play(p, dice, rolls):
@@ -27,51 +30,28 @@ def part1(p1, p2):
         
     return rolls * (s1 if winner == 2 else s2)
 
+OUTCOMES = Counter(map(sum, product(*[[1,2,3]]*3)))
+
+@lru_cache(maxsize=None)
+def quantum_dice(p1, s1, p2, s2):
+    # p1 is current player
+    if s1 >= 21:
+        return 1, 0
+    if s2 >= 21:
+        return 0, 1
+
+    win1 = win2 = 0
+    for outcome, freq in OUTCOMES.items():
+        p1_new = (p1 + outcome - 1) % 10 + 1
+        s1_new = s1 + p1_new
+        # switch players!
+        next_win2, next_win1 = quantum_dice(p2, s2, p1_new, s1_new)
+        win1 += next_win1 * freq
+        win2 += next_win2 * freq
+    return win1, win2
+        
 def part2(p1, p2):
-
-    max_score = 29
-    max_turn = 21
-    max_pos = 10
-
-    # possible outcomes of throwing the dice 3 times
-    # outcome:frequency
-    outcomes = {x:f for x, f in zip(range(3,10), [1,3,6,7,6,3,1])}
-
-    # dp[turn][p1][s1][p2][s2] = number of universes
-    dp = [[[[[0
-    for _ in range(max_score + 1)]
-    for __ in range(max_pos + 1)]
-    for ___ in range(max_score + 1)]
-    for ____ in range(max_pos + 1)]
-    for _____ in range(max_turn + 1)]
-
-    # initial state
-    dp[0][p1][0][p2][0] = 1
-    
-    win1, win2 = 0, 0
-    for last_turn in range(0, max_turn):
-        for last_p1 in range(1, max_pos + 1):
-            for last_s1 in range(max_score + 1):
-                for last_p2 in range(1, max_pos + 1):
-                    for last_s2 in range(max_score + 1):
-                        last_dp = dp[last_turn][last_p1][last_s1][last_p2][last_s2]
-                        if last_dp > 0:
-                            for outcome1, freq1 in outcomes.items():
-                                p1 = (last_p1 + outcome1 - 1) % 10 + 1
-                                s1 = last_s1 + p1
-                                if s1 >= 21:
-                                    win1 += freq1 * last_dp
-                                    continue
-                                    
-                                for outcome2, freq2 in outcomes.items():
-                                    p2 = (last_p2 + outcome2 - 1) % 10 + 1
-                                    s2 = last_s2 + p2
-                                    if s2 >= 21:
-                                        win2 += freq2 * last_dp
-                                        continue
-
-                                    dp[last_turn + 1][p1][s1][p2][s2] += freq1 * freq2 * last_dp
-    return max(win1, win2)
+    return max(quantum_dice(p1, 0, p2, 0))
 
 def main(pretty_print = True):
     def map_line(line):
