da5b58b06c0f844cd871e0df0da0fd981dbbdf57
Perinti <fperinti@deloitte.it>
2021-12-26 12:55:08 +0100

    read input day24

---
+++ 2021/solutions/day24.py
@@ -0,0 +1,27 @@
+import sys
+import pathlib
+prj_path = str(pathlib.Path(__file__).parent.parent.resolve())
+sys.path.append(prj_path)
+from utils.aoc import *
+
+def part1(data):
+    pass
+
+def part2(data):
+    pass
+
+def main(pretty_print = True):
+    def map_line(line):
+        l = line.split()
+        return (l[0], tuple([int(x) if x[0] == '-' or x.isnumeric() else x for x in l[1:]]))
+
+    data = map_input_lines(prj_path + '/input/day24.txt', map_line)
+
+    if (pretty_print):
+        print_results(1, part1, data)
+        print_results(2, part2, data)
+    else:
+        return part1(data), part2(data)
+
+if __name__ == "__main__":
+    main()
