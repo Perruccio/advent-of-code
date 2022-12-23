a270f4c0d22bf89202d7ea65a75becc6f2dca1be
Perruccio <francesco.perinti@gmail.com>
2022-12-23 13:15:49 +0100

    clean day23

--- advent_of_code/year2022/day23/solution.py
+++ advent_of_code/year2022/day23/solution.py
@@ -2,11 +2,14 @@ import pathlib
 
 from advent_of_code.utils import output as aoc_output
 from advent_of_code.utils import parse as aoc_parse
+from copy import copy
+import cProfile
 
 
 def get_input(file):
-    raw =  aoc_parse.input_as_lines(str(pathlib.Path(__file__).parent) + "/" + file)
-    return {c - r*1j for r, row in enumerate(raw) for c, point in enumerate(row) if point == "#"}
+    raw = aoc_parse.input_as_lines(str(pathlib.Path(__file__).parent) + "/" + file)
+    return {c - r * 1j for r, row in enumerate(raw) for c, point in enumerate(row) if point == "#"}
+
 
 def complex_neighbours(x):
     for dx in [-1, 0, 1]:
@@ -14,58 +17,63 @@ def complex_neighbours(x):
             if dx != 0 or dy != 0:
                 yield complex(x.real + dx, x.imag + dy)
 
-def simulate(elves, rounds, until_still):
-    directions = [1j, -1j, -1, 1]
 
+def simulate(elves, rounds=None, until_stationary=None):
+    directions = [1j, -1j, -1, 1]
+    # fmt: off
     direction_shifts = {
          1j:[-1,  0, 1 ],
         -1j:[-1,  0, 1 ],
          1 :[-1j, 0, 1j],
         -1 :[-1j, 0, 1j]
     }
+    # fmt: on
 
     round = 0
-    while round < rounds or until_still:
-        # NB {destination:elf}
-        new_elves = {}
-        still = set()
+    while until_stationary or (rounds and round < rounds):
+        # remember for each destination the elf that tried to go there
+        try_positions = {}
+        new_elves = copy(elves)
         for elf in elves:
-            # if alone don't move
+            # if alone don't move, already included in new_elves
             if all(neigh not in elves for neigh in complex_neighbours(elf)):
-                new_elves[elf] = elf
-                still.add(elf)
                 continue
             # check first possible direction
             for direction in directions:
                 if all(elf + direction + shift not in elves for shift in direction_shifts[direction]):
+                    # try this new position
                     try_pos = elf + direction
-                    if try_pos not in new_elves:
-                        new_elves[try_pos] = elf
+                    if try_pos not in try_positions:
+                        try_positions[try_pos] = elf
+                        new_elves.remove(elf)
                     else:
-                        new_elves[elf] = elf
-                        new_elves[new_elves[try_pos]] = new_elves[try_pos]
-                        new_elves[try_pos] = None
+                        # another elf tried to go there. both elves return to original position
+                        new_elves.add(try_positions[try_pos])
+                        try_positions[try_pos] = None
                     break
-            else:
-                new_elves[elf] = elf
-        
+
         round += 1
-        if until_still and len(still) == len(elves):
+        if until_stationary and len(elves) == len(new_elves):
             return round
-        elves = set(elf for elf, pos in new_elves.items() if pos is not None)
+
+        elves = new_elves
+        elves.update(elf for elf, old_elf in try_positions.items() if old_elf is not None)
         directions = directions[1:] + [directions[0]]
+
     min_x, max_x = int(min(elf.real for elf in elves)), int(max(elf.real for elf in elves))
     min_y, max_y = int(min(elf.imag for elf in elves)), int(max(elf.imag for elf in elves))
-    universe = {x + 1j*y for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)}
+    universe = {x + 1j * y for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)}
     return len(universe - elves)
+
 
 @aoc_output.pretty_solution(1)
 def part1(data):
-    return simulate(data, 10, False)
+    return simulate(data, 10)
+
 
 @aoc_output.pretty_solution(2)
 def part2(data):
-    return simulate(data, 0, True)
+    return simulate(data, until_stationary=True)
 
 
 def main():
@@ -76,8 +84,11 @@ def main():
 
 def test():
     example = get_input("example.txt")
-    assert part1(example) == 110
-    assert part2(example) == 20
+    assert part1(example) == 25
+
+    example2 = get_input("example2.txt")
+    assert part1(example2) == 110
+    assert part2(example2) == 20
 
     data = get_input("input.txt")
     assert part1(data) == 3970
@@ -87,4 +98,5 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    main()
+    # cProfile.run("main()")
