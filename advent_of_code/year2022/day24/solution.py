fbd69eccf570972145f1ce8b79c21f1c9816ab6f
Francesco <francesco.perinti@gmail.com>
2022-12-31 16:18:23 +0100

    optimize and clean day24

--- advent_of_code/year2022/day24/solution.py
+++ advent_of_code/year2022/day24/solution.py
@@ -1,8 +1,6 @@
+from advent_of_code.lib import aoc
 from advent_of_code.lib import parse as aoc_parse
 from advent_of_code.lib import math as aoc_math
-from advent_of_code.lib import aoc
-from collections import deque
-from math import lcm
 from copy import deepcopy
 
 
@@ -10,62 +8,52 @@ def get_input(file):
     raw = aoc_parse.as_lines(aoc.read_input(2022, 24, file))
     # directions map in complex plane. NB up is -1j
     directions = {">": 1, "<": -1, "^": -1j, "v": 1j}
-
-    # blizzards is a list of [position, direction] so that different blizzards can occupy
-    # the same position and position can be modified with time
-    blizzards = dict()
-    for r, line in enumerate(raw[1:]):
-        for c, point in enumerate(line[1:]):
-            if point in directions:
-                blizzards[complex(c, r)] = directions[point]
-    return blizzards, len(raw[0]) -2, len(raw) -2
+    blizzards, walls = set(), set()
+    for r, line in enumerate(raw):
+        for c, point in enumerate(line):
+            if point == "#":
+                # remove first line and first column to ease modulo operations
+                walls.add(complex(c - 1, r - 1))
+            elif point in directions:
+                # remove first line and first column to ease modulo operations
+                blizzards.add((complex(c - 1, r - 1), directions[point]))
+    # return width and height of actual rectangle where blizzards can move
+    # NB assuming only 1 line of walls in each direction...
+    return blizzards, walls, len(raw[0]) - 2, len(raw) - 2
 
 
 def simulate(data, come_back=False):
-    # get problem data
-    blizzards, width, height = deepcopy(data)
+    # info
+    blizzards_0, walls, width, height = deepcopy(data)
     start, end = -1j, width - 1 + height * 1j
-    # init
-    # t is time, blocked = {time:blocked positions}
-    t, seen = 0, set()
-    # mod_space will be used to wrap around blizzards
-    mod_space = width + 1j * height
-    # mod_time is used to capture cycle in blizzard movements
-    mod_time = lcm(width, height)
-    # bfs
-    todo = deque([(start, t)])
+    mod = width + 1j * height
+    # add walls around start and end to avoid player going out of map
+    walls.add(start - 1j)
+    walls.add(end + 1j)
     goals = [end, start, end] if come_back else [end]
+    positions, t = {start}, 0
     while goals:
-        pos, t = todo.popleft()
-
-        # check if over or go back
-        if pos == goals[0]:
-            goals.pop(0)
-            seen = set()
-            todo = deque([(pos, t)])
-            continue
 
         t += 1
-        t_mod = t % mod_time
 
-        # consider all possible moves
-        for move in [1, 1j, 0, -1, -1j]:
-            new_pos = pos + move
+        # move all blizzards, starting from original positions.
+        # This is useful to use blizzards_t as set of positions, without storing directions
+        blizzards_t = {
+            aoc_math.complex_modulo(blizz_pos + t * shift, mod) for blizz_pos, shift in blizzards_0
+        }
 
-            # check if in boundaries
-            if new_pos != start and new_pos != end and not (0 <= new_pos.real < width and 0 <= new_pos.imag < height):
-                continue
+        # move all positions
+        positions = {
+            pos + shift
+            for pos in positions
+            for shift in [1, 1j, 0, -1, -1j]
+            if pos + shift not in blizzards_t and pos + shift not in walls
+        }
 
-            # check if space is free
-            for move in [1, 1j, -1, -1j]:
-                origin = aoc_math.complex_modulo(new_pos - move * t, mod_space)
-                if origin in blizzards and blizzards[origin] == move:
-                    break
-            else:
-                if (new_pos, t_mod) not in seen:
-                    seen.add((new_pos, t_mod))
-                    todo.append((new_pos, t))
+        if goals[0] in positions:
+            positions = {goals.pop(0)}
     return t
+
 
 @aoc.pretty_solution(1)
 def part1(data):
@@ -86,11 +74,11 @@ def main():
 def test():
     example = get_input("example.txt")
     assert part1(example) == 18
-    # assert part2(example) == 54
+    assert part2(example) == 54
 
-    # data = get_input("input.txt")
-    # assert part1(data) == 228
-    # assert part2(data) == 723
+    data = get_input("input.txt")
+    assert part1(data) == 228
+    assert part2(data) == 723
 
     print("Test OK")
 
