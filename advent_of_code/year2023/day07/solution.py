44c45a6d36412c73f812be3dde1e6026cecf8056
Perruccio <francesco.perinti@gmail.com>
2023-12-07 20:29:59 +0100

    trick year 2023 day 7

--- advent_of_code/year2023/day07/solution.py
+++ advent_of_code/year2023/day07/solution.py
@@ -1,7 +1,6 @@
 from advent_of_code.lib import parse as aoc_parse
 from advent_of_code.lib import aoc
 from collections import Counter
-from functools import cmp_to_key
 
 
 def get_input(file):
@@ -23,11 +22,11 @@ def get_input(file):
 def compute_point(hand, joker):
     if joker == True:
         mx = [], ""
-        # NB remember that in case of ties, "J" (now "B") is the lowest
-        # replace it with "0"
         # brute force every possible value of "J" (now "B")
         for j in set(range(2, 9+1)) | set("ACDE"):
             new_hand = hand.replace("B", str(j))
+            # NB remember that in case of ties, "J" (now "B") is the lowest
+            # -> replace it with "0"
             point = compute_point(new_hand, False)[0], hand.replace("B", "0")
             mx = max(mx, point)
         return mx
@@ -38,14 +37,14 @@ def compute_point(hand, joker):
 
 @aoc.pretty_solution(1)
 def part1(data):
-    # given a couple (hand, bid), order by hand using cmp_hands as compare function
-    data.sort(key=lambda pair: compute_point(pair[0], False))
+    # order by point
+    data.sort(key=lambda pair: compute_point(pair[0], joker=False))
     return sum(rank * bid for rank, (hand,  bid) in enumerate(data, 1))
 
 
 @aoc.pretty_solution(2)
 def part2(data):
-    data.sort(key=lambda pair: compute_point(pair[0], True))
+    data.sort(key=lambda pair: compute_point(pair[0], joker=True))
     return sum(rank * bid for rank, (hand,  bid) in enumerate(data, 1))
 
 
