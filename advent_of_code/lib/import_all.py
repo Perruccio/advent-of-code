c06c3250faa1dfd06f838ed2178bf959bb06542c
Perruccio <francesco.perinti@gmail.com>
2023-12-31 00:38:44 +0100

    2023 25

--- advent_of_code/lib/import_all.py
+++ advent_of_code/lib/import_all.py
@@ -4,3 +4,4 @@ from advent_of_code.lib import aoc
 from advent_of_code.lib import parse as aoc_parse
 from advent_of_code.lib.geometry import Cuboid
 from advent_of_code.lib.math import *
+from math import prod
