ad6040fa81151d545c950cae30e545eb48f58247
Perruccio <francesco.perinti@gmail.com>
2024-12-12 01:46:40 +0100

    rewrite day11......

--- advent_of_code/year2024/day11/solution.py
+++ advent_of_code/year2024/day11/solution.py
@@ -6,22 +6,16 @@ def get_input(file):
     return aoc_parse.get_ints(raw)
 
 
-@lru_cache
-def blink(n):
-    if n == 0:
-        return {1:1}
-    d = aoc_math.n_digits(n)
-    if d % 2 == 0:
-        q = 10**(d//2)
-        return Counter([n//q, n%q])
-    else:
-        return {n*2024:1}
-
+@cache
 def compute(n, blinks):
-    v = {n:1}
-    for _ in range(blinks):
-        v = sum((Counter({a: b*x for a, b in blink(i).items()}) for i, x in v.items()), Counter())
-    return sum(v.values())
+    if blinks == 0:
+        return 1
+    if n == 0: return compute(1, blinks-1)
+    d = aoc_math.n_digits(n)
+    if d%2:
+        return compute(n*2024, blinks-1)
+    q = 10**(d//2)
+    return compute(n//q, blinks-1) + compute(n%q, blinks-1)
 
 
 @aoc.pretty_solution(1)
