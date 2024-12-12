f335596a67550022501d7e3b3dc13a229c63c380
Perruccio <francesco.perinti@gmail.com>
2024-12-12 23:34:43 +0100

    2024 day12 part1

---
+++ advent_of_code/year2024/day12/solution.py
@@ -0,0 +1,56 @@
+from advent_of_code.lib.all import *
+
+
+def get_input(file):
+    raw = aoc.read_input(2024, 12, file)
+    grid = aoc_parse.map_by_line(raw, list)
+    m = {i + 1j*j: x for i, line in enumerate(grid) for j, x in enumerate(line)}
+    return m
+
+
+@aoc.pretty_solution(1)
+def part1(m):
+    seen = set()
+    res = 0
+    for start in m:
+        if start in seen: continue
+        # find region with DFS
+        region = set()
+        q = [start]
+        plant = m[start]
+        while q:
+            p = q.pop()
+            region.add(p)
+            for step in (1, -1, 1j, -1j):
+                p2 = p + step
+                if p2 not in m or m[p2] != plant or p2 in region:
+                    continue
+                q.append(p2)
+        seen |= region
+        # find perimeter: count the number of outside neighbours
+        perimeter = 0
+        for p in region:
+            perimeter += sum(1 for step in (1, -1, 1j, -1j) if p + step not in region)
+        res += len(region) * perimeter
+    return res
+
+
+@aoc.pretty_solution(2)
+def part2(data):
+    ...
+
+def main():
+    data = get_input("input.txt")
+    part1(data)
+    part2(data)
+
+
+def test():
+    data = get_input("input.txt")
+    # assert part1(data) == 
+    # assert part2(data) == 
+    print("Test OK")
+
+
+if __name__ == "__main__":
+    main()
