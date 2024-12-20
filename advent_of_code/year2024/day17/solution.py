57e4fc0292e1f3aad3ae51b57d6276f854d008ac
Perruccio <francesco.perinti@gmail.com>
2024-12-20 14:56:48 +0100

    2024 day20

--- advent_of_code/year2024/day17/solution.py
+++ advent_of_code/year2024/day17/solution.py
@@ -8,41 +8,42 @@ def get_input(file):
     return reg, prog
 
 
-def combo(reg, o):
-    if 0 <= o <= 3:
-        return o
-    return reg[o-4]
-
-
 @aoc.pretty_solution(1)
 def part1(reg, prog):
-    i = 0
-    res = []
+    def combo(o):
+        if 0 <= o <= 3:
+            return o
+        return [a, b, c][o-4]
+
+    a, b, c = reg
+    i, res = 0, []
     while i < len(prog):
         p, o = prog[i], prog[i+1]
         i += 2
         if p == 0:
-            reg[0] = reg[0] // (2**combo(reg, o))
+            a = a // (2**combo(o))
         elif p == 1:
-            reg[1] ^= o
+            b ^= o
         elif p == 2:
-            reg[1] = combo(reg, o)%8
-        elif p == 3 and reg[0] != 0:
+            b = combo(o)%8
+        elif p == 3 and a != 0:
             i = o
         elif p == 4:
-            reg[1] ^= reg[2]
+            b ^= c
         elif p == 5:
-            res.append(combo(reg, o)%8)
+            res.append(combo(o)%8)
         elif p == 6:
             raise
-            reg[1] = reg[0] // (2**combo(reg, o))
+            b = a // (2**combo(o))
         elif p == 7:
-            reg[2] = reg[0] // (2**combo(reg, o))
+            c = a // (2**combo(o))
         
     return ','.join(map(str, res))
 
 @aoc.pretty_solution(2)
-def part2(data):
+def part2(reg, prog):
+    for i in range(0, len(prog), 2):
+        print(prog[i], prog[i+1])
     return
 
 
@@ -50,9 +51,13 @@ def test():
     data = get_input("input.txt")
     assert part1(*deepcopy(data)) == "1,5,3,0,2,5,2,5,3"
     # assert part2(data) == 
-    part2(data)
+    part2(*data)
     print("Test OK")
 
 
 if __name__ == "__main__":
-    test()
+    # test()
+    data = get_input("test.txt")
+    for a in range(0, 8):
+        print(f"{a=}")
+        part1([a, 0, 0], data[1])
---
+++ advent_of_code/year2024/day20/solution.py
@@ -0,0 +1,83 @@
+from advent_of_code.lib.all import *
+
+
+def get_input(file):
+    raw = aoc.read_input(2024, 20, file)
+    grid = aoc_parse.as_lines(raw)
+    m = {r+1j*c:x for r, line in enumerate(grid) for c, x in enumerate(line)}
+    return m
+
+
+def bfs(m, start):
+    # find distance from 'start' to any point (called with 'end')
+    res = {start:0}
+    q = deque([(0, t:=0, start)])
+    seen = {start}
+    while q:
+        l, _, p = q.popleft()
+        res[p] = l
+        # visit neighbours
+        for step in (1, -1, 1j, -1j):
+            new = p + step
+            if m[new] != "#" and new not in seen:
+                q.append((l+1, t:=t+1, new))
+                seen.add(new)
+    return res
+
+def dfs(m, start, end):
+    # compute path from start to end
+    q = [(0, start, [start])]
+    seen = {start}
+    while q:
+        l, p, path = q.pop()
+        if p == end:
+            return path
+        # visit neighbours
+        for step in (1, -1, 1j, -1j):
+            new = p + step
+            if m[new] != "#" and new not in seen:
+                q.append((l+1, new, path + [new]))
+                seen.add(new)
+    return []
+
+
+def count_cheats(end_dist, cheat_length, path, saving=100):
+    res = 0
+    for p in path:
+        # use cheat: manhattan distance <= cheat_length
+        for dr in range(-cheat_length, cheat_length+1):
+            # check only points with at manhattand distance at most cheat_length (included)
+            for dc in range(-(cheat_length-abs(dr)), cheat_length-abs(dr)+1):
+                l_cheat = abs(dr) + abs(dc)
+                p2 = p + dr + 1j*dc
+                # count if we save at least 'saving'
+                if p2 in end_dist and end_dist[p] - end_dist[p2] - l_cheat >= saving:
+                    res += 1
+    return res
+
+
+@aoc.pretty_solution(1)
+def part1(m):
+    start, end = map(lambda x: next(p for p in m if m[p] == x), ["S", "E"])
+    path = dfs(m, start, end)
+    end_dist = bfs(m, end)
+    return count_cheats(end_dist, 2, path)
+
+
+@aoc.pretty_solution(2)
+def part2(m):
+    start, end = map(lambda x: next(p for p in m if m[p] == x), ["S", "E"])
+    path = dfs(m, start, end)
+    end_dist = bfs(m, end)
+    return count_cheats(end_dist, 20, path)
+
+
+def test():
+    data = get_input("input.txt")
+    assert part1(deepcopy(data)) == 1369
+    assert part2(data) == 979012
+    print("Test OK")
+
+
+if __name__ == "__main__":
+    test()
